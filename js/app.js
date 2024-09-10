1ebd8d6a2d6eaec3cb884f3923f1984294cb89df
Pipiris <geimeriuteam@gmail.com>
2024-09-10 20:09:15 +0300

    Added button
    button is buttoning

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // Pagrindinis JS file'as, no clue kam darysiu, nes atskirom funkcijom vistiek atskirus scriptus naudosim :)
+
+
+const change_button = document.getElementById("change")
+change_button.addEventListener("click", change_to_mood)
+
+function change_to_mood()
+{
+    window.location.href = "html/tracker.html"
+
+}
+
+
